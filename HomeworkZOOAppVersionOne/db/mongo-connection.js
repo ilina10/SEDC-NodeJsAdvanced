2bd5206a33614de53412beb47b7f85f77c5d8e64
ilina10 <jakovcevskailina0@gmail.com>
2023-04-12 00:33:49 +0200

    updated

--- HomeworkZOOAppVersionOne/db/mongo-connection.js
+++ HomeworkZOOAppVersionOne/db/mongo-connection.js
@@ -1,7 +1,10 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
-// const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}.${process.env.MONGO_SERVER}.mongodb.net/?retryWrites=true&w=majority`;
-const uri = `mongodb+srv://jakovcevskailina10:Wir59tdUfHWy7wyn@cluster0.tfu0qkg.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}.${process.env.MONGO_SERVER}.mongodb.net/?retryWrites=true&w=majority`;
+
+//on my computer i could not run the server with the mongo uri from above so that's why i need to have second uri from below
+//but for safety on git i need to comment it while publishing
+// const uri = `mongodb+srv://jakovcevskailina10:Wir59tdUfHWy7wyn@cluster0.tfu0qkg.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
